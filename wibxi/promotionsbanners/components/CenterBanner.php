a413fd67b35284c7b9a9202f56256290c4084a7c
Mabudigital <Mabudigital@users.noreply.github.com>
2020-03-12 13:13:51 -0400

    Update CenterBanner.php
    Fix set banner height property.

--- wibxi/promotionsbanners/components/CenterBanner.php
+++ wibxi/promotionsbanners/components/CenterBanner.php
@@ -16,4 +16,17 @@ class CenterBanner extends PromoBanners {
             parent::onRun();
             $this->addCss('assets/css/centerbanner.css');
     }
+
+    public function defineProperties()
+    {
+        return [
+          'height' => [
+              'title'             => 'wibxi.promotionsbanners::lang.promobanner.height',
+              'description'       => 'wibxi.promotionsbanners::lang.promobanner.heightdescription',
+              'default'           => '150px',
+              'type'              => 'string',
+              'showExternalParam' => false
+          ],
+        ];
+    }
 }
